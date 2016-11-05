5a6eea22749748c91f83f1b30c83bd84e2ba0b1a
Eibx <jesper@eibx.com>
2016-11-05 06:34:37 +0100

    ADD: Added missing method to interface

--- Services/Interfaces/IWebSocketCore.cs
+++ Services/Interfaces/IWebSocketCore.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
+using System.Net.Sockets;
 using Rebronx.Server.Models;
 
 public interface IWebSocketCore
 {
 	List<SocketConnection> GetNewConnections();
 	List<WebSocketMessage> PollMessages();
+	void Send(Socket socket, string data);
 }
