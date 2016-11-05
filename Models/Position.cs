8031e53caa128cd7c5ae8d1c499c9acde332f09b
Eibx <jesper@eibx.com>
2016-11-05 06:32:07 +0100

    ADD: Equals method for Position model

--- Models/Position.cs
+++ Models/Position.cs
@@ -5,4 +5,14 @@ public class Position
 	public int Z { get; set; }
 	
 	public bool IsOnGround { get { return Z == 0; } }
+
+	public bool Equals(Position obj)
+	{		
+		if (obj == null) 
+		{
+			return false;
+		}
+						
+		return (X == obj.X && Y == obj.Y && Z == obj.Z);
+	}
 }
