616700e2c3fcf82ea46fe8434852e98da09ac226
Eibx <jesper@eibx.com>
2016-12-23 00:10:03 +0100

    UPD: Whitespace adjustment

--- Components/Component.cs
+++ Components/Component.cs
@@ -2,13 +2,13 @@ public class Component
 {
 	public T GetData<T>(Message message) where T : class
 	{
-			if (message == null || string.IsNullOrEmpty(message.Data))
-				return null;
+		if (message == null || string.IsNullOrEmpty(message.Data))
+			return null;
 
-			try {
-				return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(message.Data);
-			} catch {
-				return null;
-			}
+		try {
+			return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(message.Data);
+		} catch {
+			return null;
+		}
 	}
 }
