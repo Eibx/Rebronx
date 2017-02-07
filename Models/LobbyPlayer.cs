8c557774abc1e38611f88cc61b07eb33c5f5bd6f
Eibx <jesper@eibx.com>
2017-02-07 19:40:53 +0100

    ADD: Player ID to LobbyPlayer

--- Models/LobbyPlayer.cs
+++ Models/LobbyPlayer.cs
@@ -2,11 +2,13 @@ using Rebronx.Server.Models;
 
 public class LobbyPlayer
 {
+	public int Id { get; set; }
 	public string Name { get; set; }
 	public int Health { get; set; }
 
 	public LobbyPlayer(Player player)
 	{
+		Id = player.Id;
 		Name = player.Name;
 		Health = player.Health;
 	}
