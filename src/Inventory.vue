71857bc600f98d2ab386c622bced2f949829d5e5
Eibx <jesper@eibx.com>
2017-08-24 00:10:06 +0200

    ADD: Inventory component

---
+++ src/Inventory.vue
@@ -0,0 +1,126 @@
+<template>
+	<div class="inventory" v-show="isVisible">
+		<div class="inventory__container">
+			<div class="inventory__slots">
+				<div class="inventory__slot" v-for="item in items">
+					<div class="inventory__item" v-if="item !== null">
+						<div class="inventory__item-name">{{item.id}}</div>
+						<div class="inventory__item-count">{{item.count}}</div>
+						<svg class="inventory__item-image" viewBox="0 0 100 100">
+							<line x1="0" y1="0" x2="100" y2="100" stroke-width="2" stroke="#666" />
+							<line x1="0" y1="100" x2="100" y2="0" stroke-width="2" stroke="#666" />
+						</svg>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import DataService from './services/data.service.js'
+export default {
+	name: 'inventory',
+
+	data() {
+		return {
+			isVisible: false,
+			items: []
+		}
+	},
+	created() {
+		this.items = [
+			null,
+			{ id: 2, count: 1, image: 'pills' },
+			null,
+			{ id: 1, count: 1, image: 'pills' },
+			null, null, null, null,
+			null, null, null, null,
+			null, null, null, null,
+		];
+	},
+	methods: {
+	}
+}
+</script>
+
+<style>
+.inventory {
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	left: 0;
+	top: 0;
+
+	background: rgba(0, 0, 0, 0.6);
+
+	z-index: 100;
+}
+
+.inventory__container {
+	position: absolute;
+	width: 600px;
+	height: 600px;
+	left: 50%;
+	top: 50%;
+	margin-top: -300px;
+	margin-left: -300px;
+
+	background: #fff;
+	padding: 20px;
+}
+
+.inventory__slots {
+	display:flex;
+	flex-wrap:wrap;
+	border:1px solid #666;
+	border-width:1px 0px 0px 1px;
+}
+
+.inventory__slot {
+	width:25%;
+	border:1px solid #666;
+	border-width:0px 1px 1px 0px;
+	margin:0;
+	position:relative;
+}
+.inventory__slot:after {
+	display:block;
+	content:'';
+	padding-top:100%;
+}
+
+.inventory__item {
+	position:relative;
+	top:0;
+	left:0;
+	width:100%;
+	height:100%;
+}
+
+.inventory__item-image {
+	position:absolute;
+	top:0;
+	left:0;
+	width:100%;
+	height:100%;
+}
+
+.inventory__item-name {
+	position:absolute;
+	padding:2px 5px;
+	top:0;
+	left:0;
+	background:rgba(0, 0, 0, 0.6);
+	z-index:105;
+}
+
+.inventory__item-count {
+	position:absolute;
+	padding:2px 5px;
+	right:0;
+	bottom:0;
+	background:rgba(0, 0, 0, 0.5);
+	z-index:105;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Chat from './Chat.vue'
 import Login from './Login.vue'
 import Map from './Map.vue'
 import Lobby from './Lobby.vue'
+import Inventory from './Inventory.vue'
 
 import DataService from './services/data.service.js'
 import PlayerService from './services/player.service.js'
@@ -14,3 +15,4 @@ new Vue({ el: '#chat', render: r => r(Chat) });
 new Vue({ el: '#login', render: r => r(Login) });
 new Vue({ el: '#map', render: r => r(Map) });
 new Vue({ el: '#lobby', render: r => r(Lobby) });
+new Vue({ el: '#inventory', render: r => r(Inventory) });
