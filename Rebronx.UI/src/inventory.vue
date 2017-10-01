9bb8fd364a5dd1d3ee5aa51266a8afae57250e3c
Eibx <jesper@eibx.com>
2017-10-01 21:29:16 +0200

    ADD: Drag items between inventory and equipment slots

--- Rebronx.UI/src/inventory.vue
+++ Rebronx.UI/src/inventory.vue
@@ -3,7 +3,7 @@
 		<div class="inventory__container">
 			<div class="inventory__equipment">
 				<div class="inventory__equipment-armour">
-					 <div class="inventory__equipment-armour-slot inventory__equipment-armour-head">
+					 <div class="inventory__equipment-armour-slot inventory__equipment-armour-head" v-on:mousedown="beginMove(10)" v-on:mouseup="endMove(10)">
 						<div class="inventory__item" v-if="headSlot !== null">
 							<div class="inventory__item-name">{{headSlot.name}}</div>
 							<svg class="inventory__item-image" viewBox="0 0 100 100">
@@ -12,7 +12,7 @@
 							</svg>
 						</div>
 					 </div>
-					 <div class="inventory__equipment-armour-slot inventory__equipment-armour-body">
+					 <div class="inventory__equipment-armour-slot inventory__equipment-armour-body" v-on:mousedown="beginMove(11)" v-on:mouseup="endMove(11)">
 						 <div class="inventory__item" v-if="bodySlot !== null">
 							<div class="inventory__item-name">{{bodySlot.name}}</div>
 							<svg class="inventory__item-image" viewBox="0 0 100 100">
@@ -21,7 +21,7 @@
 							</svg>
 						</div>
 					 </div>
-					 <div class="inventory__equipment-armour-slot inventory__equipment-armour-legs">
+					 <div class="inventory__equipment-armour-slot inventory__equipment-armour-legs" v-on:mousedown="beginMove(12)" v-on:mouseup="endMove(12)">
 						 <div class="inventory__item" v-if="legsSlot !== null">
 							<div class="inventory__item-name">{{legsSlot.name}}</div>
 							<svg class="inventory__item-image" viewBox="0 0 100 100">
@@ -35,7 +35,7 @@
 					<div class="inventory__equipment-player-character"></div>
 				</div>
 				<div class="inventory__equipment-weapons">
-					<div class="inventory__equipment-weapons-primary">
+					<div class="inventory__equipment-weapons-primary" v-on:mousedown="beginMove(20)" v-on:mouseup="endMove(20)">
 						<div class="inventory__item" v-if="primaryWeapon !== null">
 							<div class="inventory__item-name">{{primaryWeapon.name}}</div>
 							<svg class="inventory__item-image" viewBox="0 0 100 100">
@@ -44,21 +44,19 @@
 							</svg>
 						</div>
 					</div>
-					<div class="inventory__equipment-weapons-secondary">
-						<div class="inventory__equipment-weapons-primary">
-							<div class="inventory__item" v-if="secondaryWeapon !== null">
-								<div class="inventory__item-name">{{secondaryWeapon.name}}</div>
-								<svg class="inventory__item-image" viewBox="0 0 100 100">
-									<line x1="0" y1="0" x2="100" y2="100" stroke-width="2" stroke="#666" />
-									<line x1="0" y1="100" x2="100" y2="0" stroke-width="2" stroke="#666" />
-								</svg>
-							</div>
+					<div class="inventory__equipment-weapons-secondary" v-on:mousedown="beginMove(30)" v-on:mouseup="endMove(30)">
+						<div class="inventory__item" v-if="secondaryWeapon !== null">
+							<div class="inventory__item-name">{{secondaryWeapon.name}}</div>
+							<svg class="inventory__item-image" viewBox="0 0 100 100">
+								<line x1="0" y1="0" x2="100" y2="100" stroke-width="2" stroke="#666" />
+								<line x1="0" y1="100" x2="100" y2="0" stroke-width="2" stroke="#666" />
+							</svg>
 						</div>
 					</div>
 				</div>
 			</div>
 			<div class="inventory__slots">
-				<div class="inventory__slot" v-for="(item, index) in items" v-on:mousedown="beginMove(index)" v-on:mouseup="endMove(index)">
+				<div class="inventory__slot" v-for="(item, index) in items" v-on:mousedown="beginMove(index+100)" v-on:mouseup="endMove(index+100)">
 					<div class="inventory__item" v-if="item !== null">
 						<div class="inventory__item-name">{{item.name}}</div>
 						<div class="inventory__item-count" v-if="item.count > 1">{{item.count}}</div>
@@ -116,12 +114,15 @@ export default {
 			this.headSlot = null;
 			this.bodySlot = null;
 			this.legsSlot = null;
+			this.primaryWeapon = null;
+			this.primaryAmmo = null;
+			this.secondaryWeapon = null;
+			this.secondaryAmmo = null;
 
 			for (var i = 0; i < ids.length; i++) {
 				var itemId = ids[i][0];
 				var itemCount = ids[i][1];
-				var itemPos = ids[i][2];
-				var itemSlot = ids[i][3];
+				var itemSlot = ids[i][2];
 
 				var item = this.getItem(itemId);
 				var uiItem = null;
@@ -131,9 +132,10 @@ export default {
 					uiItem = { name: "unknown " + itemId, count: itemCount };
 				}
 
-				if (itemPos > -1 && itemSlot == -1) {
-					newItems[itemPos] = uiItem;
-				} else if (itemSlot > -1 && itemPos == -1) {
+				if (itemSlot >= 100) {
+					newItems[itemSlot-100] = uiItem;
+				} else {
+
 					if (itemSlot == 10) {
 						this.headSlot = uiItem;
 					} else if (itemSlot == 11) {
@@ -149,10 +151,8 @@ export default {
 					} else if (itemSlot == 31) {
 						this.secondaryAmmo = uiItem;
 					}
+
 				}
-
-				
-
 			}
 			
 			this.items = newItems;
@@ -167,12 +167,16 @@ export default {
 			return null;
 		},
 		beginMove(index) {
-			for (var i = 0; i < this.items.length; i++) {
-				if (this.items[index] === null) {
-					this.moveItem = null;
-					return;
-				} else {
-					this.moveItem = index;
+			if (index < 100) {
+				this.moveItem = index;
+			} else {
+				for (var i = 0; i < this.items.length; i++) {
+					if (this.items[index] === null) {
+						this.moveItem = null;
+						return;
+					} else {
+						this.moveItem = index;
+					}
 				}
 			}
 		},
@@ -184,10 +188,13 @@ export default {
 			if (this.moveItem == index) {
 				dataService.send('inventory', 'equip', { from: this.moveItem, to: null });
 			} else {
-				dataService.send('inventory', 'reorder', { current: this.moveItem, new: index });
+				dataService.send('inventory', 'reorder', { from: this.moveItem, to: index });
 			}
 
 			this.moveItem = null;
+		},
+		unequip(slot) {
+			alert(slot);
 		}
 	}
 }
@@ -268,6 +275,17 @@ export default {
 }
 
 .inventory__equipment-weapons-primary:after {
+	display:block;
+	content:'';
+	padding-top:100%;
+}
+
+.inventory__equipment-weapons-secondary {
+	border:1px solid #666;
+	margin-bottom:3px;	
+}
+
+.inventory__equipment-weapons-secondary:after {
 	display:block;
 	content:'';
 	padding-top:100%;
