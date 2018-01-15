0a122e46ebec770642ca1cbd56e751f2aff2ae11
Eibx <jesper@eibx.com>
2018-01-15 01:28:34 +0100

    ADD: First test of item rendering

--- Rebronx.UI/src/main.js
+++ Rebronx.UI/src/main.js
@@ -8,12 +8,14 @@ import Store from './store.vue'
 
 import DataService from './services/data.service.js'
 import PlayerService from './services/player.service.js'
+import RenderService from './services/render.service.js'
 
 window.mapData = require("../../Rebronx.Data/map.json");
 window.itemData = require("../../Rebronx.Data/items.json");
 
 window.dataService = new DataService();
 window.playerService = new PlayerService();
+window.renderService = new RenderService();
 
 new Vue({ el: '#chat', render: r => r(Chat) });
 new Vue({ el: '#login', render: r => r(Login) });
